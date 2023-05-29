1ad30648bfeebcaa504f22047ade4b53e4f29e7c
Michael J. Larocca <michaellarocca@live.com>
2023-05-29 15:27:21 -0400

    Changed function order to maintain best practices

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,36 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState("light");
 
-  const saveThemeToLocalStorage = () => {
-    localStorage.setItem('theme', theme);
-  };
+	function getThemeFromLocalStorage() {
+		const savedTheme = localStorage.getItem("theme");
+		if (savedTheme) {
+			setTheme(savedTheme);
+		}
+	}
 
-  const getThemeFromLocalStorage = () => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      setTheme(savedTheme);
-    }
-  };
+	function toggleTheme() {
+		setTheme((prevTheme) => {
+			const newTheme = prevTheme === "light" ? "dark" : "light";
+			localStorage.setItem("theme", newTheme);
+			return newTheme;
+		});
+	}
 
-  useEffect(() => {
-    getThemeFromLocalStorage();
-  }, [theme]);
+	useEffect(() => {
+		getThemeFromLocalStorage();
+	}, [theme]);
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => {
-      const newTheme = prevTheme === 'light' ? 'dark' : 'light';
-      localStorage.setItem('theme', newTheme);
-      return newTheme;
-    });
-  };
-
-  return (
-    <div className={`ctn ${theme}`}>
-      <div className={`app ${theme}`}>
-        <h1>Current theme: {theme}</h1>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`ctn ${theme}`}>
+			<div className={`app ${theme}`}>
+				<h1>Current theme: {theme}</h1>
+				<button onClick={toggleTheme}>Toggle Theme</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
